d157f309d7331acb02f56a665b7e25cccf26333c
anechka396 <anechka3960@gmail.com>
2019-09-24 13:26:49 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	var bracketsMap = new Map();
+	var stack = [];
+	
+	bracketsConfig.forEach(function(bracketsPair){ 
+		bracketsMap.set(bracketsPair[0], bracketsPair[1])
+	});
+	for (var i = 0; i < str.length; i++) {
+		bracketsMap.get(stack[stack.length-1]) === str.charAt(i) ? stack.pop() : stack.push(str.charAt(i));
+	}
+	
+	return stack.length === 0;
 }
